96b28d54b4b07ae5acff8049a09a83022d0b35e2
Renan da Silva <renan_paula2011@hotmail.com>
2019-06-27 21:29:22 -0300

    Close #6

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <title>Sistema de Login</title>
+        <style>
+            #alerta,#caixaRegistro,#caixaSenha{
+                display: none;
+            }    
+        </style>
     </head>
     <body class="bg-dark">
         <main class="container mt-4">
@@ -229,5 +234,31 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script>
+            //$(document).ready(function(){});
+            //JQuery
+            $(function(){
+                //Mostra caixa de senha
+                    $("#btnEsqueci").click(function(){
+                    $("#caixaLogin").hide();
+                    $("#caixaSenha").show();
+                });
+                //Volta para o Login
+                    $("#btnVoltar").click(function(){
+                    $("#caixaLogin").show();
+                    $("#caixaSenha").hide();
+                });
+                //Mostra caixa Registrar no Login
+                    $("#btnRegistrar").click(function(){
+                    $("#caixaLogin").hide();
+                    $("#caixaRegistro").show();
+                    });
+                //Voltar Login
+                    $("#btnEntrarRegistrado").click(function(){
+                    $("#caixaRegistro").hide();
+                    $("#caixaLogin").show();
+                    });
+            });
+        </script>
     </body>
 </html>
