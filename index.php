49609f00ec2e43cab9a853f1c6f50d914bb6589a
renan <renan@172.20.22.248>
2019-07-04 21:07:07 -0300

    Atualização do README Informações sobra a aula 04

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+        <!-- CSS JQuery validate-->
+        <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+        
         <title>Sistema de Login</title>
         <style>
             #alerta,#caixaRegistro,#caixaSenha{
@@ -55,7 +57,7 @@
                                    required minlength="6">
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group mt-5">
                             <div class="custom-control custom-checkbox">
                                 <input type="checkbox" 
                                        name="lembrar"
@@ -143,22 +145,22 @@
                                    placeholder="Confirmar a senha" required
                                    minlength="6">
                         </div>
-                        
+
                         <!-- Checkbox de aceite dos termos -->
-                        <div class="form-group">
+                        <div class="form-group mt-5">
                             <div class="custom-control custom-checkbox">
-                            <input type="checkbox"
-                                   name="concordar"
-                                   class="custom-control-input"
-                                   id="checkConcordar">
-                            <label for="checkConcordar"
-                                   class="custom-control-label">
-                                Eu concordo com os
-                                <a href="#">termos e condições</a>
-                            </label>
+                                <input type="checkbox"
+                                       name="concordar"
+                                       class="custom-control-input"
+                                       id="checkConcordar">
+                                <label for="checkConcordar"
+                                       class="custom-control-label">
+                                    Eu concordo com os
+                                    <a href="#">termos e condições</a>
+                                </label>
                             </div>
                         </div>
-                        
+
                         <!-- botão enviar-->
                         <div class="form-group">
                             <input type="submit" 
@@ -167,19 +169,19 @@
                                    value=":: Registrar ::"
                                    class="btn btn-primary btn-block">
                         </div>
-                        
+
                         <!-- Já Registrado -->
                         <div class="form-group">
                             <p class="text-center">
                                 Já registrado?
                                 <a href="#"
                                    id="btnEntrarRegistrado">
-                                       Entrar aqui.
+                                    Entrar aqui.
                                 </a>
                             </p>
-                            
+
                         </div>
-                        
+
                     </form>
 
                 </div>
@@ -234,30 +236,72 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <!-- plugin JQuery validate-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
         <script>
             //$(document).ready(function(){});
             //JQuery
-            $(function(){
+            $(function () {
                 //Mostra caixa de senha
-                    $("#btnEsqueci").click(function(){
+                $("#btnEsqueci").click(function () {
                     $("#caixaLogin").hide();
                     $("#caixaSenha").show();
                 });
                 //Volta para o Login
-                    $("#btnVoltar").click(function(){
+                $("#btnVoltar").click(function () {
                     $("#caixaLogin").show();
                     $("#caixaSenha").hide();
                 });
                 //Mostra caixa Registrar no Login
-                    $("#btnRegistrar").click(function(){
+                $("#btnRegistrar").click(function () {
                     $("#caixaLogin").hide();
                     $("#caixaRegistro").show();
-                    });
+                });
                 //Voltar Login
-                    $("#btnEntrarRegistrado").click(function(){
+                $("#btnEntrarRegistrado").click(function () {
                     $("#caixaRegistro").hide();
                     $("#caixaLogin").show();
-                    });
+                });
+                //Validação com JQuery
+                //verificação certo azul
+                jQuery.validator.setDefaults({
+                    debug: false,
+                    success: "valid"
+});
+                $("#formLogin").validate();
+                $("#formSenha").validate();
+                $("#formRegistro").validate({
+                    rules:{
+                        senhaUsuarioConfirmar:{
+                            equalTo:"#senhaUsuario"
+                        }
+                    }
+                });
+                    
+            });
+
+            /*
+             * Translated default messages for the jQuery validation plugin.
+             * Locale: PT_BR
+             */
+            jQuery.extend(jQuery.validator.messages, {
+                required: "Este campo &eacute; requerido.",
+                remote: "Por favor, corrija este campo.",
+                email: "Por favor, forne&ccedil;a um endere&ccedil;o eletr&ocirc;nico v&aacute;lido.",
+                url: "Por favor, forne&ccedil;a uma URL v&aacute;lida.",
+                date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
+                dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
+                number: "Por favor, forne&ccedil;a um n&uacute;mero v&aacute;lido.",
+                digits: "Por favor, forne&ccedil;a somente d&iacute;gitos.",
+                creditcard: "Por favor, forne&ccedil;a um cart&atilde;o de cr&eacute;dito v&aacute;lido.",
+                equalTo: "Por favor, forne&ccedil;a o mesmo valor novamente.",
+                accept: "Por favor, forne&ccedil;a um valor com uma extens&atilde;o v&aacute;lida.",
+                maxlength: jQuery.validator.format("Por favor, forne&ccedil;a n&atilde;o mais que {0} caracteres."),
+                minlength: jQuery.validator.format("Por favor, forne&ccedil;a ao menos {0} caracteres."),
+                rangelength: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1} caracteres de comprimento."),
+                range: jQuery.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1}."),
+                max: jQuery.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
+                min: jQuery.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
             });
         </script>
     </body>
